69f2e0584d48e2a2855e9657045bf2510b401a19
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2022-01-29 23:35:18 +0530

    heroku deploy try 5

--- trekthehill/settings.py
+++ trekthehill/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
